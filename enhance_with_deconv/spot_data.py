120df2b52453123e41c54c65c6eb8da27d7bf8d4
Elena Pascal <elenadpascal@gmail.com>
2022-09-20 11:37:36 +0100

    move files

--- enhance_with_deconv/spot_data.py
+++
@@ -1,6 +0,0 @@
-# Graeme's data spot example
-# normally this info would be read programmatically from a dials file
-
-import numpy as np
-intensity_map = np.load('data/pixel_array.npy')[0]
-s1_vector = np.mean(np.load('data/s1_vectors.npy'), axis=0)
