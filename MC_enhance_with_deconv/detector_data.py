9077a5c9d96a8c9a6de8faee5c98cecda735183b
Elena Pascal <elenadpascal@gmail.com>
2022-09-20 11:30:15 +0100

    remove old files

--- MC_enhance_with_deconv/detector_data.py
+++
@@ -1,9 +0,0 @@
-# Pilatus detector parameters, Graeme's data
-# normally this info would be read programmatically from a dials file
-
-import numpy as np
-
-pixel_size = (0.172, 0.172)  # mm
-depth = 1  # mm
-mu = 0.6245476712834831  # mm^-1
-normal = np.array([-0.00186461, 0.00883524, -0.99995923])
